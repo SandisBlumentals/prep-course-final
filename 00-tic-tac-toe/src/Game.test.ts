a7d714804eb19c193dc440caad66580daf7ff759
SandisBlumentals <162323022+SandisBlumentals@users.noreply.github.com>
2024-05-29 10:40:01 +0000

    Game complete

--- 00-tic-tac-toe/src/Game.test.ts
+++ 00-tic-tac-toe/src/Game.test.ts
@@ -13,4 +13,135 @@ describe("Tic-Tac-Toe", () => {
     expect(game.getWinner()).toBe("-");
     expect(game.isTie()).toBe(false);
   });
+  
+  it(`should place X on the clicked cell`, () => {
+    const game = new Game();
+
+    game.onClick(0);
+
+    expect(game.getCells()).toEqual([
+      "X", "-", "-",
+      "-", "-", "-",
+      "-", "-", "-"
+    ]);
+    expect(game.getTurn()).toBe("O");
+    expect(game.getWinner()).toBe("-");
+    expect(game.isTie()).toBe(false);
+  });
+
+  it(`should place O on the clicked cell`, () => {
+    const game = new Game();
+
+    game.onClick(0);
+    game.onClick(1);
+
+    expect(game.getCells()).toEqual([
+      "X", "O", "-",
+      "-", "-", "-",
+      "-", "-", "-"
+    ]);
+    expect(game.getTurn()).toBe("X");
+    expect(game.getWinner()).toBe("-");
+    expect(game.isTie()).toBe(false);
+  });
+  
+  it(`should not place a different value when its X or O clicked cell`, () => {
+    const game = new Game();
+
+    game.onClick(0);
+    game.onClick(0);
+
+    expect(game.getCells()).toEqual([
+      "X", "-", "-",
+      "-", "-", "-",
+      "-", "-", "-"
+    ]);
+    expect(game.getTurn()).toBe("O");
+    expect(game.getWinner()).toBe("-");
+    expect(game.isTie()).toBe(false);
+  });
+
+  it(`should declare a winner by same symbol row`, () => {
+    const game = new Game();
+
+    game.onClick(0);
+    game.onClick(3);
+    game.onClick(1);
+    game.onClick(4);
+    game.onClick(2);
+
+    expect(game.getCells()).toEqual([
+      "X", "X", "X",
+      "O", "O", "-",
+      "-", "-", "-"
+    ]);
+
+    expect(game.getWinner()).toBe("X");
+    expect(game.isTie()).toBe(false);
+  });
+
+  it(`should declare a winner by same symbol collomn`, () => {
+    const game = new Game();
+
+    game.onClick(0);
+    game.onClick(1);
+    game.onClick(3);
+    game.onClick(2);
+    game.onClick(6);
+
+    expect(game.getCells()).toEqual([
+      "X", "O", "O",
+      "X", "-", "-",
+      "X", "-", "-"
+    ]);
+
+    expect(game.getWinner()).toBe("X");
+    expect(game.isTie()).toBe(false);
+  });
+
+  it(`should declare a winner by same symbol diagonal`, () => {
+    const game = new Game();
+
+    game.onClick(0);
+    game.onClick(1);
+    game.onClick(4);
+    game.onClick(5);
+    game.onClick(8);
+
+    expect(game.getCells()).toEqual([
+      "X", "O", "-",
+      "-", "X", "O",
+      "-", "-", "X"
+    ]);
+
+    expect(game.getWinner()).toBe("X");
+    expect(game.isTie()).toBe(false);
+  });
+
+  it(`should declare a tie`, () => {
+    const game = new Game();
+
+    game.onClick(0); //X
+    game.onClick(1); //O
+
+    game.onClick(2); //x
+    game.onClick(4); //o
+
+    game.onClick(3); //x
+    game.onClick(6); //o
+
+    game.onClick(5); //x
+    game.onClick(8); //o
+
+    game.onClick(7); //x
+
+    expect(game.getCells()).toEqual([
+      "X", "O", "X",
+      "X", "O", "X",
+      "O", "X", "O"
+    ]);
+
+    expect(game.getWinner()).toBe("-");
+    expect(game.isTie()).toBe(true);
+  });
 });
--- 00-tic-tac-toe/src/Game.ts
+++ 00-tic-tac-toe/src/Game.ts
@@ -1,27 +1,85 @@
 export type XO = "X" | "O" | "-";
 
 export class Game {
+  gameFieldCells: XO[] = ["-", "-", "-", "-", "-", "-", "-", "-", "-"];
+  gameEnd:boolean = false;
+  turn: XO = "X";
+
   getCells(): XO[] {
-    return ["X", "-", "-", "-", "-", "-", "-", "-", "-"];
+    return this.gameFieldCells;
   }
 
   getTurn(): XO {
-    return "X";
+    return this.turn;
   }
 
   getWinner(): XO {
+    const cells = this.gameFieldCells;
+    
+    //rows
+    if (cells[0] === cells[1] && cells[0] === cells[2] && (cells[0] === "X" || cells[0] === "O")){
+      this.gameEnd = true;
+      return this.gameFieldCells[0];
+    }
+    if (cells[3] === cells[4] && cells[3] === cells[5] && (cells[3] === "X" || cells[3] === "O")){
+      this.gameEnd = true;
+      return this.gameFieldCells[3];
+    }
+    if (cells[6] === cells[7] && cells[6] === cells[8] && (cells[6] === "X" || cells[6] === "O")){
+      this.gameEnd = true;
+      return this.gameFieldCells[6];
+    }
+    
+    //colloms
+    if (cells[0] === cells[3] && cells[0] === cells[6] && (cells[0] === "X" || cells[0] === "O")){
+      this.gameEnd = true;
+      return this.gameFieldCells[0];
+    }
+    if (cells[1] === cells[4] && cells[1] === cells[7] && (cells[1] === "X" || cells[1] === "O")){
+      this.gameEnd = true;
+        return this.gameFieldCells[1];
+    }
+    if (cells[2] === cells[5] && cells[2] === cells[8] && (cells[2] === "X" || cells[2] === "O")){
+      this.gameEnd = true;
+        return this.gameFieldCells[2];
+    }
+  
+    //diagonals
+    if (cells[0] === cells[4] && cells[0] === cells[8] && (cells[0] === "X" || cells[0] === "O")){
+      this.gameEnd = true;
+      return this.gameFieldCells[0];
+    }
+    if (cells[2] === cells[4] && cells[2] === cells[6] && (cells[2] === "X" || cells[2] === "O")){
+      this.gameEnd = true;
+      return this.gameFieldCells[2];
+    }
+
     return "-";
   }
 
   isTie(): boolean {
+    const cells = this.gameFieldCells;
+    var turnCount = 0;
+    for (var i = 0; i < cells.length; i++){
+      if (cells[i] !== "-" && this.gameEnd === false){
+        turnCount++;
+      }
+    }
+    if (turnCount === 9) return true;
     return false;
   }
 
   onClick(i: number): void {
-    console.log(`cell ${i} clicked`);
+    if (this.gameEnd === false){
+      if (this.gameFieldCells[i] === "-"){
+        this.gameFieldCells[i] = this.turn;
+        if (this.turn === "X") this.turn = "O"
+        else this.turn = "X";
+      }
+    }
   }
 
   restart(): void {
-    console.log("restart called");
+    location.reload();
   }
 }
